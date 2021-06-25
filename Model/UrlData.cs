730b8e38c5c5fc1139e184e90bcf9c72dfe922e5
dlozina <dino.lozina@itsistemi.com>
2021-06-25 10:51:52 +0200

    Add project files.

---
+++ Model/UrlData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shortener.Service.Model
+{
+    public class UrlData
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ Services/UrlHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Shortener.Service.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shortener.Service.Services
+{
+    public class UrlHelper : IUrlHelper
+    {
+        // Transform the "Id" property on this object into a short string
+        public string GetShortUrl(int id)
+        {
+            return WebEncoders.Base64UrlEncode(BitConverter.GetBytes(id));
+        }
+
+        // Reverse short url text back into an interger Id
+        public int GetId(string shortUrl)
+        {
+            return BitConverter.ToInt32(WebEncoders.Base64UrlDecode(shortUrl));
+        }
+    }
+}
